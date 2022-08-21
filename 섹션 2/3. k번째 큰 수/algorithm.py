a7ef71780d2cd6d4adc0efa7b87e0e12e95a8e5c
Jay9393 <tlswo1313@gmail.com>
2022-08-21 17:26:04 +0900

    Q3 k번째 큰 수

--- 섹션 2/3. k번째 큰 수/algorithm.py
+++ 섹션 2/3. k번째 큰 수/algorithm.py
@@ -1,8 +1,19 @@
+from pickle import TRUE
+
+
 for i in range(1, 6):
-    f = open(f"섹션 2/1. k번째 큰 수/in{i}.txt", "r")
-    line = f.readline()
-    n, k=map(int, line.split())
+    f = open(f"섹션 2/3. K번째 큰 수/in{i}.txt", "r")
+    lines = f.readlines()
     f.close()
-    f = open(f"섹션 2/1. k번째 큰 수/out{i}.txt", "r")
-    answer = int(f.readline())
-    f.close()
+    n, k = map(int, lines[0].split())
+    cards = list(map(int, lines[1].split()))
+    result = set()
+    for j in range(n):
+        for l in range(j+1, n):
+            for m in range(l+1, n):
+                result.add(cards[j] + cards[l] + cards[m])
+    result = list(result)
+    result.sort(reverse=True)
+    print(result[k-1])
+    print(f"==================={i}=================")
+    
